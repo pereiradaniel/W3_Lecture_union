d9525e7591bcde2baa5671efcdf05ca3c6183bee
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-25 12:28:02 -0500

    Bytes

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 union ID
 {
-	char m_strId[20];
-	long long m_iId;
+	char m_strId[20];	// 20 bytes
+	long long m_iId;	// 8 bytes
 };
 
 int main()
 {
-	ID theId;
+	ID theId;			// 20 bytes
 }
